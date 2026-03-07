cf62a7743e6d97f4bc0d319c7f16dc460cc523e0
Zunaid <133950183+ZunaidChowdhury@users.noreply.github.com>
2026-03-07 10:57:59 +0600

    sign in part done

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-// console.log('hello');
+console.log('index.js connected');
+
+document.querySelector('#sign-in-btn').addEventListener('click', e => {
+    e.preventDefault();
+    const username = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+
+    if (username === 'admin' && password === 'admin123') {
+        window.location.assign('./pages/home.html');
+    }
+    else {
+        alert('Sign in failed. Please check your username and password.');
+    }
+});
