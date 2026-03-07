5ee99b3f180081e879e5224d83cff47cf5481891
Zunaid <133950183+ZunaidChowdhury@users.noreply.github.com>
2026-03-07 22:04:55 +0600

    tab ui done

--- scripts/home.js
+++ scripts/home.js
@@ -1,1 +1,5 @@
 console.log('Home connected.');
+
+const tabBtnClasses = `w-[7.5rem] btn bg-white text-text-secondary text-base font-medium hover:bg-[#4a00ffcc] hover:text-white border border-[#e4e4e7FF] transition-all duration-300 rounded-[.25rem] px-4 py-3`;
+const tabActiveBtnClasses = `w-[7.5rem] btn bg-theme-primary text-white text-base font-semibold hover:bg-[#4a00ffcc] border-none transition-all duration-300 rounded-[.25rem] px-4 py-3`;
+
